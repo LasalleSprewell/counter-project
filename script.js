e4fa1a56cfe235d149c9eaa44e17dbcda73e73e8
LasalleSprewell <133616710+LasalleSprewell@users.noreply.github.com>
2023-11-16 06:21:52 -0500

    Make Buttons clickable

---
+++ script.js
@@ -0,0 +1,16 @@
+// Initial Number Count
+let currentNum = 0;
+let decButton = document.getElementById("decrease")
+let inButton = document.getElementById("increase")
+let resButton = document.getElementById("reset")
+
+
+resButton.addEventListener ("click", function() {
+    console.log("YEA!")
+});
+decButton.addEventListener ("click", function() {
+    console.log("DUMB!")
+});
+inButton.addEventListener ("click", function() {
+    console.log("YSTUPID!")
+});
