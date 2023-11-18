f5d0338f0fdd50bce941332deaa05bdea14e4832
LasalleSprewell <133616710+LasalleSprewell@users.noreply.github.com>
2023-11-17 20:39:51 -0500

    Comment out Javascript File

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 // Initial Number Count
 let currentNumber = 0;
 const numberUpdate = document.getElementById("currentNum");
+
+// Decrese Button Setup
 let decButton = document.getElementById("decrease");
+
+// Increase Button Setup
 let inButton = document.getElementById("increase");
+
+// Reset Button Setup
 let resButton = document.getElementById("reset");
 
-
+// Color Change Setup
 const red = ()=>{
     if(numberUpdate){
         numberUpdate.style.color = "red";
     }
 };
-
 const green = () =>{
     if(numberUpdate){
         numberUpdate.style.color = "green";
@@ -21,7 +26,7 @@ const black = () =>{
     if(numberUpdate){
         numberUpdate.style.color = "black";
     }
-}
+// Color Change Function
 const colorChange = ()=>{
     if (currentNumber > 0){
         green();
@@ -34,6 +39,7 @@ const colorChange = ()=>{
     }
 }
 
+// Function to Increse and Decrease the Number
 const resetButtonClick = resButton.addEventListener ("click", function() {
     currentNumber = 0;
     updateNumber();
@@ -55,7 +61,3 @@ const updateNumber = ()=>{
     if (numberUpdate){
         numberUpdate.textContent = currentNumber;
     }};
-
- 
-
-
